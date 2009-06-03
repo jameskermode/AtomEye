16bc1f07fe6f41a8527beebd0a5b968ec611ef10
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2009-06-03 15:49:48 +0000

    Major overhaul of atomeyelib interface. Now much closer to being thread safe!
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/AtomEye@5153 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- A3/atomeyelib.h
+++ A3/atomeyelib.h
@@ -1,11 +1,26 @@
 /* atomeye C api to be called from Python */
 
-int atomeyelib_main(int argc, char* argv[], void (*onclick)(int atom), 
-		    void (*onclose)(void), void (*onadvance)(char *instr), int *done_init);
-int atomeyelib_run_command(int iw, char *line, char **outstr);
-int atomeyelib_help(int iw, char *line, char **outstr);
-int atomeyelib_redraw(int iw);
-int atomeyelib_close(int iw);
-int atomeyelib_load_libatoms(int iw, Atoms *atoms, char *title, char **outstr);
+#ifndef _ATOMEYELIB_H
+#define _ATOMEYELIB_H
 
-/* int atomeyelib_set_output(int on_off); */
+#define ATOMEYELIB_REDRAW 1
+#define ATOMEYELIB_RUN_COMMAND 2
+#define ATOMEYELIB_LOAD_ATOMS 3
+
+extern void (*atomeyelib_on_click_atom)(int iw, int atom);
+extern void (*atomeyelib_on_close)(int iw);
+extern void (*atomeyelib_on_advance)(int iw, char *instr);
+extern void (*atomeyelib_on_new)(int iw);
+
+int atomeyelib_init(int argc, char* argv[], void* data);
+void atomeyelib_set_handlers(void (*on_click)(int iw, int atom), 
+			     void (*on_close)(int iw), 
+			     void (*on_advance)(int iw, char *instr),
+			     void (*on_new)(int iw));
+int atomeyelib_open_window(int icopy);
+void atomeyelib_set_title(int iw, char *title);
+int atomeyelib_treatevent(int iw);
+int atomeyelib_queueevent(int iw, int event, char *instr, void *data, char *outstr);
+
+
+#endif
