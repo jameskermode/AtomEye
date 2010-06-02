bb26493378fded009d1f27696f4ac1ede8adc40d
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-06-02 11:10:47 +0000

    Overhauled Makefile system to remove the dependency on QUIP. If $QUIP_ARCH is set, we try to use QUIP Makefiles, otherwise Makefile.config can be used instead.
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/AtomEye@6555 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Makefile
+++ Makefile
@@ -3,8 +3,22 @@ ifeq (${QUIP_ROOT},)
   export QUIP_ROOT
 endif
 
-all:
-	$(MAKE) -f Makefile.atomeye -I${QUIP_ROOT} -I${QUIP_ROOT}/Makefiles -I${QUIP_ROOT}/build.${QUIP_ARCH} all
+ifneq (${QUIP_ARCH},)
+  HAVE_QUIP = 1
+  export HAVE_QUIP
+endif
 
-.DEFAULT:
+all:
+ifeq (${HAVE_QUIP},1)
+	$(MAKE) -f Makefile.atomeye -I${QUIP_ROOT} -I${QUIP_ROOT}/Makefiles -I${QUIP_ROOT}/build.${QUIP_ARCH}
+else
+	$(MAKE) -f Makefile.atomeye
+endif
+
+.DEFAULT: 
+ifeq (${HAVE_QUIP},1)
 	$(MAKE) -f Makefile.atomeye -I${QUIP_ROOT} -I${QUIP_ROOT}/Makefiles -I${QUIP_ROOT}/build.${QUIP_ARCH} $@
+else
+	$(MAKE) -f Makefile.atomeye $@
+endif
+
--- Python/setup.py
+++ Python/setup.py
@@ -19,19 +19,53 @@
 
 import sys, os, re, string
 from numpy.distutils.core import setup, Extension
+from distutils.sysconfig import parse_makefile
 
 major, minor = sys.version_info[0:2]
 if (major, minor) < (2, 4):
     sys.stderr.write('Python 2.4 or later is needed to use this package\n')
     sys.exit(1)
 
-atomeye_dir =  os.path.join(os.getcwd(), '..')
-atomeye_libs = ['m', 'Xpm', 'Xext', 'X11', 'mpi', 'png', 'z', 'jpeg', 'history', 'ncurses',
-                'netcdf', 'hdf5', 'hdf5_hl', 'readline', 'curl']
-atomeye_libdirs = ['/opt/local/lib']
-atomeye_extra_link_args = ['-framework Accelerate']
+def expand_addsuffix(s):
+    add_suffix = re.compile(r'\$[\(\{]addsuffix (.*?),(.*?)[\)\}]')
+    try:
+        m = add_suffix.search(s)
+    except TypeError:
+        return s
+    if m:
+        while m is not None:
+            suffix, files = m.groups()
+            s = add_suffix.sub(' '.join([f + suffix for f in files.split()]),s,1).strip()
+            m = add_suffix.search(s)
+    return s
 
-os.system('cd .. && make atomeyelib')
+def expand_addprefix(s):
+    add_prefix =  re.compile(r'\$[\(\{]addprefix (.*?),(.*?)[\}\)]')
+    try:
+        m = add_prefix.search(s)
+    except TypeError:
+        return s
+    if m:
+        while m is not None:
+            prefix, files = m.groups()
+            s = add_prefix.sub(' '.join([prefix + f for f in files.split()]),s,1).strip()
+            m = add_prefix.search(s)
+    return s
+
+atomeye_dir =  os.path.join(os.getcwd(), '..')
+os.system('cd %s && make -p atomeyelib > %s/make.log' % (atomeye_dir, os.getcwd()))
+makefile = parse_makefile('make.log')
+os.unlink('make.log')
+
+syslibs = makefile['SYSLIBS']
+syslibs = expand_addprefix(syslibs)
+syslibs = expand_addsuffix(syslibs)
+syslibs = syslibs.split()
+
+atomeye_libs = ['m', 'Xpm', 'Xext', 'X11', 'mpi', 'png', 'z', 'jpeg', 'history', 'ncurses', 'readline'] + [ f[2:] for f in syslibs if f.startswith('-l') ]
+atomeye_libdirs = [ f[2:] for f in syslibs if f.startswith('-L') ]
+atomeye_extra_link_args = [ f for f in syslibs if not f.startswith('-l') and not f.startswith('-L')]
+
 
 setup(name='atomeye',
       py_modules = ['atomeye'],
